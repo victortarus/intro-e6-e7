a67ad685e1fff087bf4898ccc6742cc81a3e7100
victor tarus <victorkibet25@gmail.com>
2018-08-30 14:09:23 +0300

    spacing is kind diff

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-Template literals
+/Template literals/
 let name1 = "hello";
 let name2 = "world";
-
-
-/new version/
-const fullName = `${name1} ${name2}`;
-console.log(fullName);
+let num1 = 2;
+let num2 = 4;
+//using backticks
+const addNumbers = `${num1 +num2}`;
+console.log(addNumbers)
+    //const fullName = `${name1} ${name2}`;
+    //console.log(fullName);
+let example = `${name1 }
+ ${name2}`;
+document.getElementById('example').innerText = example;
